4e24b7c5484408001cccc3704d6924b692fba500
Brittain Hard <brittainhard@gmail.com>
2017-10-09 10:46:09 -0500

    New book!

---
+++ cookbook/data_structures/sequence_to_variables.py
@@ -0,0 +1,8 @@
+p = (4, 5)
+x, y = p
+
+data = ["ACME", 50, 91.1, (2012, 12, 21)]
+name, shares, price, date = data
+
+name2, shares2, price2, (year, mon, day) = data
+
---
+++ cookbook/data_structures/tests/test_data_structures.py
@@ -0,0 +1,6 @@
+from ..sequence_to_variables import *
+
+def test_sequence_date():
+    assert year == 2012
+    assert day == 21
+    assert mon == 12
