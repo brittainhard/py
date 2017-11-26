a9f2c15d2d177e7b5023e9c285ac1abe4059b84f
Brittain Hard <brittainhard@gmail.com>
2017-11-26 13:43:20 -0600

    More chainmap examples

--- cookbook/data_structures/chainmaps.py
+++ cookbook/data_structures/chainmaps.py
@@ -1,6 +1,33 @@
 from collections import ChainMap
 
+
+"""
+If you delete an item from a dictionary, it always deletes it from the latest
+dictionary.
+
+This will make those dictionaries incongruent.
+"""
 a = {"x": 1, "z": 3}
 b = {"y": 2, "z": 4}
 c = ChainMap(a, b)
+del c["z"]
 print(c)
+
+
+"""
+Scoped variables?
+
+Lets you keep a dict that has a chain of variables, kind of like a stack.
+"""
+values = ChainMap()
+values["x"] = 1
+values = values.new_child()
+values["x"] = 2
+values = values.new_child()
+values["x"] = 3
+print(values["x"])
+print(values)
+print(values.parents["x"])
+print(values.parents)
+print(values.parents.parents["x"])
+print(values.parents.parents)
