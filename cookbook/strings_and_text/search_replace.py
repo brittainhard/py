5985b3f5e0adbf747f90dd7b7b79cc85d1370253
Brittain Hard <brittainhard@gmail.com>
2017-12-11 12:19:45 -0600

    Finished 2.5

--- cookbook/strings_and_text/search_replace.py
+++ cookbook/strings_and_text/search_replace.py
@@ -7,10 +7,21 @@ dates = "Today is 11/27/2012. Pycon starts 3/13/2013."
 a = text.replace("yeah", "yep")
 
 # Example using re.sub.
-# pattern = re.compile(r'(\d+)/(\d+)/(\d+)')
-# replace = re.compile(r'\3-\1-\2')
 pattern = re.compile(r'(\d+)/(\d+)/(\d+)')
 replace = r'\3-\1-\2'
 sub = re.sub(pattern, replace, dates)
 sub2 = pattern.sub(replace, dates)
 
+# This can be used to find out how many substitutions were made.
+nums, n = pattern.subn(replace, dates)
+
+# Use of callback statements
+from calendar import month_abbr
+
+
+def change_date(m):
+    mon_name = month_abbr[int(m.group(1))]
+    return '{} {} {}'.format(m.group(2), mon_name, m.group(3))
+
+
+callback = pattern.sub(change_date, dates)
