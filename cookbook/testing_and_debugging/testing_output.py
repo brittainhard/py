b236f791968caaa5b2094eef391ed0366afe84bb
Brittain Hard <brittainhard@gmail.com>
2018-02-04 20:20:37 -0600

    First example in testing chapter. Need to add notebook notes.

---
+++ cookbook/testing_and_debugging/testing_output.py
@@ -0,0 +1,22 @@
+def urlprint(protocol, host, domain):
+    url = '{}://{}.{}'.format(protocol, host, domain)
+    print(url)
+
+
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
+
+
+class TestURLPrint(TestCase):
+
+    def test_url_gets_to_stdout(self):
+        protocol = 'https'
+        host = 'www'
+        domain = 'example.com'
+        expected_url = '{}://{}.{}\n'.format(protocol, host, domain)
+        print(expected_url)
+
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            urlprint(protocol, host, domain)
+            self.assertEqual(fake_out.getvalue(), expected_url)
