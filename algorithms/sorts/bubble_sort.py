257cdacbdda20ace39aaa8f56c62ddd5bdfaf394
Brittain Hard <brittainhard@gmail.com>
2018-02-09 16:06:48 -0600

    Bubble sort

---
+++ algorithms/sorts/bubble_sort.py
@@ -0,0 +1,17 @@
+import random
+
+
+
+def bubble_sort(items):
+    for y in range(len(items)):
+        for x in range(0, len(items)):
+            a = items[x]
+            if x == len(items) - 1:
+                break
+            b = items[x + 1]
+            if a > b:
+                items[x] = b
+                items[x + 1] = a
+    return items
+
+a = [x for x in range(5000)]
